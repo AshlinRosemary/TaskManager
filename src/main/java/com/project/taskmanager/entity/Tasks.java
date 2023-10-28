3ee5d3d1ed829e8f4511591b0fe1edc0d55d2280
AshlinRosemary <ashlinrosemary@gmail.com>
2023-10-28 12:24:10 +0400

    Project first commit

---
+++ src/main/java/com/project/taskmanager/entity/Tasks.java
@@ -0,0 +1,27 @@
+package com.project.taskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="Tasks")
+public class Tasks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    private boolean completed;
+}
